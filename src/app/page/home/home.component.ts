61d90d89de4b1a9a991e1e27d7e19920bbf14cb2
thanh605 <thanhchinguyen605@gmail.com>
2023-11-14 16:38:41 +0700

    update homepage

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as data from './data.json'
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  listCategory: { name: string, url: string, quantity: number }[] = [];
+
+  ngOnInit(): void {
+    this.listCategory = data?.catergory;
+    console.log(this.listCategory);
+
+  }
 
 }
