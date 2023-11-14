ea80077c6959c8610921890333faf021d39c442f
FE-dev605 <thanhchinguyen605@gmail.com>
2023-11-14 21:55:35 +0700

    update navbar and redo scss

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as navItem from 'src/app/services/data-navbar.json'
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -8,11 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
   listDemos: string[] = [];
   listPage: string[] = [];
+  listNavbar: any[] = [];
+  navbarImage: string = '';
   constructor() { }
 
   ngOnInit(): void {
     this.listDemos = ['Modern', 'Standard', 'Minimal', 'Vintage', 'Classic', 'Classic', 'Trendy', 'Elegant', 'Refined', 'Fashion'];
-    this.listPage = ['User', 'FAQ', 'Privacy Policy', 'Term & Condition', 'Contact Us', 'Checkout', 'Collection', 'Category', 'Order', '404']
+    this.listPage = ['User', 'FAQ', 'Privacy Policy', 'Term & Condition', 'Contact Us', 'Checkout', 'Collection', 'Category', 'Order', '404'];
+    this.listNavbar = navItem?.header?.item;
+    this.navbarImage = navItem?.header?.image;
   }
 
 }
