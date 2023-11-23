49a30d29303a0d778b8e2cdf38f1238e370d5efb
FE-dev605 <thanhchinguyen605@gmail.com>
2023-11-23 16:27:57 +0700

    update homepage

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -6,26 +6,61 @@ import * as data from './data.json'
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit, AfterViewInit {
+  // slider collection 
   slider: any;
   items: any;
   dots: any;
   active: any;
+  // slider flash sales
+  currentImage: any;
+  listFlashSales: any[] = [];
+
+  listCategory: { name: string, url: string, quantity: number }[] = [];
+  listCarousel: string[] = [];
+  listTopProduct: any;
+  listCollection: string[] = [];
+  listFeature: any[] = [];
+
   constructor(
     private elRef: ElementRef
   ) { }
   ngAfterViewInit(): void {
+    this._initSliderCollection();
+    this._initSliderFlashSales();
+  }
+
+
+
+  ngOnInit(): void {
+    this.listCategory = data?.catergory;
+    this.listCarousel = data?.carousel;
+    this.listCollection = data?.collection?.url;
+    this.currentImage = data?.product?.topProduct[0].image;
+    this.listFeature = data?.feature;
+    this._initListTopProduct();
+  }
+  private _initSliderCollection() {
     this.slider = this.elRef.nativeElement.querySelector('.slider .list');
     this.items = this.elRef.nativeElement.querySelectorAll('.slider .list .item');
     this.dots = this.elRef.nativeElement.querySelectorAll('.slider .dots li');
     this.active = 0;
   }
 
-  listCategory: { name: string, url: string, quantity: number }[] = [];
-  listCarousel: string[] = [];
+  private _initSliderFlashSales() {
 
-  ngOnInit(): void {
-    this.listCategory = data?.catergory;
-    this.listCarousel = data?.carousel;
+  }
+  private _initListTopProduct() {
+    this.listTopProduct = data?.product?.topProduct;
+    this.listTopProduct.map((itm: any) => {
+      return itm.description = this._handleLongString(itm.description);
+    })
+  }
+
+  private _handleLongString(txt: string): string {
+    if (txt.length - 1 > 37) {
+      return txt.slice(-txt.length, 37) + '...';
+    }
+    return txt;
   }
 
   private _reloadSlider() {
